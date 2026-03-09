ae741c085d40f7eed776529fab81952854226582
daniell096 <daniell096@yahoo.com>
2026-03-09 20:25:57 +0200

    feat:Corecteaza valoarea bonusului la 5

--- Calculator.java
+++ Calculator.java
@@ -1,1 +1,1 @@
-"public class Calculator {public int aduna(int a, int b) {private static final int BONUS = 2;return a + b + bonus;}public static void main(String[] args) {Calculator calc = new Calculator();System.out.println("Rezultat: " + calc.aduna(5, 10));}}" 
+"public class Calculator {public int aduna(int a, int b) {private static final int BONUS = 5;return a + b + bonus;}public static void main(String[] args) {Calculator calc = new Calculator();System.out.println("Rezultat: " + calc.aduna(5, 10));}}" 
